ca2a32f35ed3c0a30e10dbc9b0300789dc0bc286
khaled.dev <salehkhaled.dev@gmail.com>
2023-09-18 11:37:19 +0300

    useEffect dependancies

--- src/Home.js
+++ src/Home.js
@@ -8,6 +8,8 @@ const Home = () => {
         { title: 'web dev tips', body: "dummy data", author: "fattah", id: 3 },
     ]);
 
+const [name, setName] = useState('khaled');
+
 const handleDelete = (id) => {
     const newBlogs = blogs.filter(blogs => blogs.id !== id);
     setBlogs(newBlogs);
@@ -15,14 +17,15 @@ const handleDelete = (id) => {
 
 useEffect(() => {
     console.log("useEffect run");
+    console.log(name);
     console.log(blogs)
-});
+}, [name]);
 
     return (
         <div className="home">
             <BlogList blogs = {blogs} title="All Blogs" handleDelete = {handleDelete} />
-            <BlogList blogs = {blogs.filter((blog) => blog.author === "khaled")} title="Khaled's blogs" />
-
+            <button onClick = {() => setName('fattah')} >Change Name</button>
+            <p>{ name }</p>
         </div>
     );
 }
