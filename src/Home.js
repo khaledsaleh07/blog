09017548ec4f07713a9d022ffdebea965666b1b4
khaled.dev <salehkhaled.dev@gmail.com>
2023-09-18 10:01:45 +0300

    reusing props

--- src/Home.js
+++ src/Home.js
@@ -4,13 +4,15 @@ import BlogList from './BlogList';
 const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: "dummy data", author: "khaled", id: 1 },
-        { title: 'Welcome Party', body: "dummy data", author: "kareen", id: 2 },
+        { title: 'Welcome Party', body: "dummy data", author: "khaled", id: 2 },
         { title: 'web dev tips', body: "dummy data", author: "fattah", id: 3 },
     ]);
 
     return (
         <div className="home">
             <BlogList blogs = {blogs} title="All Blogs" />
+            <BlogList blogs = {blogs.filter((blog) => blog.author === "khaled")} title="Khaled's blogs" />
+
         </div>
     );
 }
