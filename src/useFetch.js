99de6ef49c42843c5366e85c96e8b1b71a883320
khaled.dev <salehkhaled.dev@gmail.com>
2023-09-20 10:15:57 +0300

    useEffect cleanup

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,9 +6,12 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
+        //abort controller in JS
+        const abortCont = new AbortController();
+
         // just to simulate real data fetching, we added the setTimeout
         setTimeout(() => {
-            fetch(url)
+            fetch(url, { signal: abortCont.signal })
                 .then(res => {
                     if(!res.ok) {
                         throw Error('response is not ok');
@@ -26,6 +29,8 @@ const useFetch = (url) => {
                     setError(err.message);
                 })
         }, 1000);
+
+        return () => abortCont.abort();
     }, [url]);
 
     return { data, isPending, error };
